6cd458b52a7760859a0f132e9577b05877110f25
Alexander Mokeichuk <154593925+AlexanderMokeichuk@users.noreply.github.com>
2026-05-18 16:23:45 +0600

    style: format eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,12 +7,7 @@ const eslintConfig = defineConfig([
   ...nextTs,
 
   // Override default ignores of eslint-config-next.
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 
   // Custom rules.
   {
